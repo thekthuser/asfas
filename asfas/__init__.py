2f9117300ac0a5cc61a728a98f97860046d0bef3
thekthuser <thekthuser@thekthuser.com>
2016-04-18 17:23:02 -0400

    moved app config from run.py to __init__.py

--- asfas/__init__.py
+++ asfas/__init__.py
@@ -7,13 +7,23 @@ from flask.ext.login import LoginManager
 from flask_wtf.csrf import CsrfProtect
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bcrypt import Bcrypt
+from flask.ext.uploads import UploadSet, configure_uploads, IMAGES
+import socket
 
 app = Flask(__name__)
+
+#if __name__ == '__main__':
+if socket.gethostname() == 'kth.site':
+    app.config.from_object('dev_settings')
+else:
+    app.config.from_object('prod_settings')
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 CsrfProtect(app)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+images = UploadSet('images', IMAGES)
+configure_uploads(app, images)
 
 import asfas.admin_routes
--- run.py
+++ run.py
@@ -2,13 +2,6 @@
 # -*- coding: utf-8 -*-
 # run.py
 
-from flask import Flask
-import socket
 from asfas import app
 
-if __name__ == '__main__':
-    if socket.gethostname() == 'kth.site':
-        app.config.from_object('dev_settings')
-    else:
-        app.config.from_object('prod_settings')
-    app.run()
+app.run()
